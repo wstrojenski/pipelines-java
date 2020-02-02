c39755002801add878499abd712e5b16dd1e47ce
Wojtek Strojenski <39518433+wstrojenski@users.noreply.github.com>
2020-02-02 18:40:53 +0100

    Update MyTest.java

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 
 public class MyTest {
     @Test
-    public void test_method_1() {
+    public void test_method_1() 
         Demo d = new Demo();
         d.DoSomething(true);
     }
